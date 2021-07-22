16e613cd916a86cab86c812dc4c95d25dd5e3303
Sammy840 <64469303+Sammy840@users.noreply.github.com>
2021-07-22 12:37:24 +0100

    Family

---
+++ obj.js
@@ -0,0 +1,49 @@
+let thisMessage = document.getElementById("message");
+            
+// check the empty input
+function checkEmptyInput(){
+  let isEmpty = false,
+      fname = document.getElementById("fname").value,
+      lname = document.getElementById("lname").value,
+      age = document.getElementById("age").value;
+            
+  if(fname === ""){
+    thisMessage.innerHTML = "First Name Cannot Be Empty!";
+      isEmpty = true;
+    }
+      else if(lname === ""){
+        thisMessage.innerHTML = "Last Name Cannot Be Empty!";
+      isEmpty = true;
+    }
+      else if(age === ""){
+        thisMessage.innerHTML = "Age cannot Be Empty!";
+      isEmpty = true;
+    }
+      return isEmpty;
+};  
+
+
+let movies = [];
+// example {id:1592304983049, title: 'Deadpool', year: 2015}
+const addMovie = (ev)=>{
+    ev.preventDefault();  //to stop the form submitting
+    let movie = {
+        id: Date.now(),
+        title: document.getElementById('title').value,
+        year: document.getElementById('yr').value
+    }
+    movies.push(movie);
+    document.forms[0].reset(); // to clear the form for the next entries
+    //document.querySelector('form').reset();
+
+    //for display purposes only
+    console.warn('added' , {movies} );
+    let pre = document.querySelector('#msg pre');
+    pre.textContent = '\n' + JSON.stringify(movies, '\t', 2);
+
+    //saving to localStorage
+    localStorage.setItem('MyMovieList', JSON.stringify(movies) );
+}
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('btn').addEventListener('click', addMovie);
+});
